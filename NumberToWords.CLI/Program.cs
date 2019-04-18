a080c9fc6c9891456e4b5727c313f2904eaa1fb4
Matias Guiomar Henschel <matias.henschel@capgemini.com>
2019-04-18 10:05:02 -0300

    Renamed solution. Added CLI project to help with testing and debugging.

---
+++ NumberToWords.CLI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumberToWords.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number: ");
+            bool success = false;
+            string spell = string.Empty;
+            while (!success)
+            {
+                string numberStr = Console.ReadLine();
+                if (long.TryParse(numberStr, out long number))
+                {
+                    spell = NumberConversor.Convert(number);
+                    success = true;
+                }
+                else
+                    Console.Write("Type a valid integer number: ");
+            }
+            Console.WriteLine(spell);
+        }
+    }
+}
